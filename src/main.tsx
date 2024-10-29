e9a74a17b66383e22b0e02401175dc55f485aba2
Afshin Ghezeli <a.g.afshin@gmail.com>
2024-10-29 12:07:02 +0330

    Downgrade Chakra UI to 2.10.3

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { Provider } from "@/components/ui/provider"
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider>
+    <ChakraProvider>
       <App />
-    </Provider>
-  </React.StrictMode>,
-)
+    </ChakraProvider>
+  </React.StrictMode>
+);
