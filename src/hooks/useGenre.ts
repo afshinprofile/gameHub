87282cc2e03d90375f57bf7d5ca1330ef712c9e4
Afshin Ghezeli <a.g.afshin@gmail.com>
2024-11-12 11:00:00 +0330

    Refactor useGenre hook

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,4 +1,6 @@
-import genres from "../data/genres";
+import apiClient from "@/services/api-client";
+import { useQuery } from "@tanstack/react-query";
+import { FetchResponse } from "./useData";
 export interface Genre {
   id: number;
   name: string;
@@ -6,6 +8,13 @@ export interface Genre {
 }
 
 // const useGenre = () => useData<Genre>("/genres");
-const useGenre = () => ({ data: genres, isLoading: false, error: null });
+const useGenre = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+    gcTime: 24 * 60 * 60 * 1000,
+  });
 
 export default useGenre;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import "@/index.css";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import theme from "./theme";
-import "@/index.css";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+    },
+  },
+});
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
